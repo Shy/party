f0023136379d5ea48c846ffbae3175ecb58e28ef
Shy Ruparel <Shy@users.noreply.github.com>
2022-12-29 00:05:37 -0500

    Dynamic Static Sites

--- netlify/edge-functions/attendee_status.js
+++ netlify/edge-functions/attendee_status.js
@@ -1,51 +1,44 @@
 const API_ENDPOINT =
     "https://app.superblocks.com/agent/v1/workflows/0e26c575-344c-483c-a848-f76e78ee4c86?environment=production";
 
-export default async (request) => {
-    console.log("hello");
+export default async (request, context) => {
     const url = new URL(request.url);
-
-    // Look for the "?method=transform" query parameter, and return if we don't find it
-    if (url.pathname.startsWith("/rsvp/")) {
-        const SUPERBLOCKS_PROD_WF = process.env.SUPERBLOCKS_PROD_WF;
-        split_path = url.pathname.split("/");
-
-        if (split_path[split_path.length - 1] == "") {
-            event_junction_pub_id = split_path[split_path.length - 2];
-        } else {
-            event_junction_pub_id = split_path[split_path.length - 1];
-        }
-
-        let response;
-        try {
-            response = await fetch(API_ENDPOINT, {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                    Authorization: "Bearer " + SUPERBLOCKS_PROD_WF,
-                },
-                body: JSON.stringify({ event_junction_pub_id: event_junction_pub_id }),
-            });
-        } catch (err) {
-            return {
-                statusCode: err.statusCode || 500,
-                body: JSON.stringify({
-                    error: err.message,
-                }),
-            };
-        }
-
-        const page = await response.JSON();
-
-        // Search for the placeholder
-        const regex = /STATUS_UNKNOWN/i;
-
-        // Replace the content with the current location
-        const updatedPage = page.replace(regex, response["data"][0]["rsvp"]);
-
-        // Return the response
-        return new Response(updatedPage, response);
+    const env_vars = Deno.env.toObject();
+    const split_path = url.pathname.split("/");
+    let event_junction_pub_id;
+    if (split_path[split_path.length - 1] == "") {
+        event_junction_pub_id = split_path[split_path.length - 2];
     } else {
-        return;
+        event_junction_pub_id = split_path[split_path.length - 1];
     }
+    let SBresponse;
+    try {
+        SBresponse = await fetch(API_ENDPOINT, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: "Bearer " + env_vars["SUPERBLOCKS_PROD_WF"],
+            },
+            body: JSON.stringify({ event_junction_pub_id: event_junction_pub_id }),
+        });
+    } catch (err) {
+        return {
+            statusCode: err.statusCode || 500,
+            body: JSON.stringify({
+                error: err.message,
+            }),
+        };
+    }
+
+    const response = await context.next();
+    const page = await response.text();
+    const jsonData = await SBresponse.json();
+
+    // Search for the placeholder
+    const regex = /STATUS_UNKNOWN/i;
+
+    const updatedPage = page.replace(regex, jsonData["data"][0]["rsvp"]);
+
+    // Return the response
+    return new Response(updatedPage, response);
 };
