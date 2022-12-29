3c0612a696415d1f741b726ea0c0e6e6d3c6fc9a
Shy Ruparel <Shy@users.noreply.github.com>
2022-12-28 22:17:22 -0500

    Added robots

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, request, url_for
+from flask import render_template, request, url_for, send_from_directory, request
 from app import app, db
 from app.models import Event, Attendee, EventAttendeeJunction
 from functools import wraps
@@ -39,8 +39,14 @@ def textInvites(attendee, event_junction_public_id):
 
 
 @app.route("/")
+@app.route("/index/")
 def index():
     return render_template("index.html")
+
+
+@app.route("/robots.txt")
+def static_from_root():
+    return send_from_directory(app.static_folder, request.path[1:])
 
 
 @app.route("/events/<event_public_id>/", methods=["GET"])
