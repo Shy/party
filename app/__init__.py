f004e979180a0e7832e096a757d42af7a1339dc7
Shy Ruparel <Shy@users.noreply.github.com>
2023-01-26 22:12:32 -0500

    cleaning

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,10 @@
 from flask import Flask
+from flask_minify import minify
 from flask_sqlalchemy import SQLAlchemy
 from config import Config
 
 app = Flask(__name__)
+minify(app=app, html=True, js=True, cssless=True)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 
