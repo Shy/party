73932cbaab40a3b10e8da63dc5a98b20340f2d6a
Shy Ruparel <Shy@users.noreply.github.com>
2023-02-18 10:28:44 -0500

    logging shit

--- netlify/functions/form-handler.js
+++ netlify/functions/form-handler.js
@@ -35,7 +35,10 @@ exports.handler = async (event, _context, callback) => {
 
     updatedRsvp = client
         .query(query, values)
-        .then((result) => result.rows[0].rsvp) // your callback here
+        .then((result) => {
+            console.log(result.rows);
+            result.rows[0].rsvp;
+        }) // your callback here
         .catch((e) => console.error(e.stack)); // your callback here
 
     let message = "Error. Ping Shy to fix things.";
