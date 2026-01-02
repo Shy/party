d2a0e6f2ad2cbfabc40b5cc74be26cc3dca9035e
Shy Ruparel <Shy@users.noreply.github.com>
2026-01-01 21:56:57 -0500

    fix line breaks

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,8 @@
 from flask import Flask
-from flask_minify import minify
 from flask_sqlalchemy import SQLAlchemy
 from config import Config
 
 app = Flask(__name__)
-minify(app=app, html=True, js=True, cssless=True)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 app.add_template_global(name="PARTY_NAME", f=app.config["PARTY_NAME"])
